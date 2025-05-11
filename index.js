4bbea7891fa8bba76b1b71c0d715e1e13778c959
Gabriel Farías Ravanal <gabriel@lekinstrumentos.cl>
2025-05-11 17:14:53 -0400

    2.0.3 types

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
-const telbit = require("./lib/telbit");
+const Telbit = require("./lib/telbit");
+
+/**
+ * 
+ * @param {string} BOT_TOKEN 
+ * @returns {import("./lib/types/index")}
+ */
+const telbit = (BOT_TOKEN) =>
+{
+    try
+    {
+        return new Telbit(BOT_TOKEN);
+    }
+    catch(err)
+    {
+        throw err;
+    }
+}
+
 module.exports = telbit;
---
+++ lib/types/index.d.ts
@@ -0,0 +1,6 @@
+// index.d.ts
+export { default as Chat } from "../chat";
+export { default as Message } from "../message";
+export { default as User } from "../user";
+export { default as Permissions } from "../permissions";
+export { default as Telbit } from "../telbit";
