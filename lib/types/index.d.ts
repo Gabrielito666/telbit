5b25ca70e9f3b43149e1b0ae4cf937410a1d813f
Gabriel Farías Ravanal <gabriel@lekinstrumentos.cl>
2025-05-20 18:02:13 -0400

    2.0.9

--- lib/types/index.d.ts
+++ lib/types/index.d.ts
@@ -5,6 +5,11 @@ import Chat from "../chat";
 import User from "../user";
 import Message from "../message";
 
+export type { default as Telbit } from "../telbit";
+export type { default as Chat } from "../chat";
+export type { default as User } from "../user";
+export type { default as Message } from "../message";
+
 export type ChatPermissions = {
     can_send_messages?: boolean;
     can_send_media_messages?: boolean;
